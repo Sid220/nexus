57bac6f4716abb2724583827ef6cd48a888aa006
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2024-11-04 17:15:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ from basecampapi.basecampapi import MessageBoard
 
 
 class BCManager:
-    # credentials = {
-    #     "account_id": "5855125",
-    #     "client_id": "1b53154c47ecb8e11e9a44c527a8abca14cea1fa",
-    #     "client_secret": "5e8e7245114032ddb000de2029216bc6e48bc31d",
-    #     "redirect_uri": "https://b621-50-170-108-90.ngrok-free.app/auth",
-    #     "refresh_token": "BAhbB0kiAbB7ImNsaWVudF9pZCI6IjFiNTMxNTRjNDdlY2I4ZTExZTlhNDRjNTI3YThhYmNhMTRjZWExZmEiLCJleHBpcmVzX2F0IjoiMjAzNC0xMS0wNFQxMzo0NTozOFoiLCJ1c2VyX2lkcyI6WzUwMTMwMzE4XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWU1N2E5YjNjNDdjMzNmMGQ4YmIzN2JkOWE5NDQzMmEifQY6BkVUSXU6CVRpbWUNjaghwAMwZLYJOg1uYW5vX251bWkCVQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgc0EDoJem9uZUkiCFVUQwY7AEY=--0efd4326320a9acc4c29925929a2371515d96414"
-    # }
-
     def __init__(self, config):
         self.bc = Basecamp(credentials=config.to_dict())
 
